741cf6bf6532b5553763d96cbf9154d9e28c68b3
SyedAleezHassan <syedaleezhassan@gmal.com>
2024-02-14 00:05:04 +0500

    add line

--- main.dart
+++ main.dart
@@ -1,5 +1,7 @@
 void main() {
   List<int> num = [1, 2, 3, 4, 5, 6, 7, 8];
+  print("Even numbers in this list are ");
+  
   for (int i = 0; i < num.length; i++) {
     if (num[i] % 2 == 0) {
       //print("even numbers are $i");
