bc8a2292d07bf3a3f32069c1935bae20e2094f48
SyedAleezHassan <syedaleezhassan@gmal.com>
2024-02-14 00:00:12 +0500

    even number find

---
+++ main.dart
@@ -0,0 +1,12 @@
+void main() {
+  List<int> num = [1, 2, 3, 4, 5, 6, 7, 8];
+  for (int i = 0; i < num.length; i++) {
+    if (num[i] % 2 == 0) {
+      //print("even numbers are $i");
+      print(num[i]);
+      
+    }
+     
+  }
+ 
+}
