bba12b5e7e1ef500afd9b9f50313f713637e059d
Syed Aleez Hassan <155354112+SyedAleezHassan@users.noreply.github.com>
2024-06-27 12:53:48 +0500

    Update cube.dart

--- cube.dart
+++ cube.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 void main() {
-  print("Enterr any num to find cube");
+  print("Enterr any number to find cube");
   String val = stdin.readLineSync()!;
   int userInp = int.parse(val);
   int finalRes = userInp * userInp * userInp;
